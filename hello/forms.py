5c381107c4889568474c859b50349d56fff164fb
Michael Fogleman <fogleman@gmail.com>
2013-10-25 21:51:20 -0400

    updated for Flask-WTF 0.9+

--- hello/forms.py
+++ hello/forms.py
@@ -1,4 +1,5 @@
-from flask.ext.wtf import Form, TextField, validators
+from flask.ext.wtf import Form
+from wtforms import TextField, validators
 
 class CommentForm(Form):
     text = TextField('Comment', [validators.Required()])
